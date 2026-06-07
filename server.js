840e5585d95271067b26b5e05c5b712551505002
Jasprit45 <singhsardarjasprit@gmail.com>
2026-06-08 00:38:56 +0530

    Created APIs related to users

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import { inngest, functions } from "./inngest/index.js"
 import showRouter from './Routes/showRoutes.js';
 import bookingRouter from './Routes/bookingRoutes.js';
 import adminRouter from './Routes/adminRoutes.js';
+import userRouter from './Routes/userRoutes.js';
 
 
 const app = express();
@@ -28,6 +29,7 @@ app.use('/api/inngest',serve({ client: inngest, functions }));
 app.use('/api/show', showRouter);
 app.use('/api/booking', bookingRouter);
 app.use('/api/admin',adminRouter);
+app.use('/api/user',userRouter);
 
 await connectDB();
 
