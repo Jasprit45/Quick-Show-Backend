6373e42648e1b53109144a3e4b31d398fd883820
Jasprit45 <singhsardarjasprit@gmail.com>
2026-06-12 23:10:58 +0530

    Changed the smtp service

--- configs/nodemailer.js
+++ configs/nodemailer.js
@@ -1,14 +1,20 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
+   service: 'gmial',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
 
+console.log(JSON.stringify(process.env.SMTP_USER));
+console.log(JSON.stringify(process.env.SMTP_PASS));
+await transporter.verify();
+console.log("SMTP verified");
 
 const sendEmail  = async ({to,subject,body}) => {
     const response = await transporter.sendMail({
